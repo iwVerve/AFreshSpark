2a9e2c227a4421249599624aa94e6ced5cd34405
iwVerve <thedolphiner@gmail.com>
2024-07-18 22:40:16 +0200

    rewrite build.zig, put raylib artifact back

--- build.zig
+++ build.zig
@@ -1,22 +1,29 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // TARGET
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // OPTIONS
     const static = b.option(bool, "static", "Build standalone executable") orelse false;
     const options = b.addOptions();
     options.addOption(bool, "static", static);
     options.addOption([]const u8, "dll_name", "game");
 
-    const raylib_dep = b.dependency("raylib", .{
-        .target = target,
-        .optimize = .ReleaseFast,
-        // Build raylib in shared library when hotreloading to share raylib state between executable and game library.
-        .shared = !static,
-    });
-    const raylib = raylib_dep.artifact("raylib");
+    // DEPENDENCIES
+    const raylib = blk: {
+        const raylib_dep = b.dependency("raylib", .{
+            .target = target,
+            .optimize = .ReleaseFast,
+            // Build raylib in shared library when hotreloading to share raylib state between executable and game library.
+            .shared = !static,
+        });
+        break :blk raylib_dep.artifact("raylib");
+    };
+    b.installArtifact(raylib);
 
+    // COMPILE STEPS
     var dll = b.addSharedLibrary(.{
         .name = "game",
         .root_source_file = b.path("src/Game.zig"),
@@ -24,31 +31,39 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     dll.linkLibrary(raylib);
-
     const dll_install = b.addInstallArtifact(dll, .{});
 
-    const exe = b.addExecutable(.{
-        .name = if (static) "static" else "dynamic",
+    const reload_step = b.step("reload", "Build the dll");
+    reload_step.dependOn(&dll_install.step);
+
+    const dynamic_exe = b.addExecutable(.{
+        .name = "dynamic",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.linkLibrary(raylib);
-    exe.root_module.addOptions("config", options);
-    if (!static) {
-        exe.step.dependOn(&dll_install.step);
-    }
-    b.installArtifact(exe);
+    dynamic_exe.linkLibrary(raylib);
+    dynamic_exe.root_module.addOptions("config", options);
+    dynamic_exe.step.dependOn(&dll_install.step);
+    b.installArtifact(dynamic_exe);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const static_exe = b.addExecutable(.{
+        .name = "static",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    static_exe.linkLibrary(raylib);
+    static_exe.root_module.addOptions("config", options);
+    b.installArtifact(static_exe);
+
+    // RUN STEPS
+    const run_cmd = b.addRunArtifact(dynamic_exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    const reload_step = b.step("reload", "Build the dll");
-    reload_step.dependOn(&dll_install.step);
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
