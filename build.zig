fcd8cb53ed4aeb431dc7479b140937896efa9cf0
iwVerve <thedolphiner@gmail.com>
2024-07-21 18:08:36 +0200

    rewrite build.zig, fail at building for web

--- build.zig
+++ build.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
+const Build = std.Build;
+const Compile = std.Build.Step.Compile;
+const ResolvedTarget = std.Build.ResolvedTarget;
+const OptimizeMode = std.builtin.OptimizeMode;
+const Options = std.Build.Step.Options;
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // TARGET
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -11,7 +16,7 @@ pub fn build(b: *std.Build) void {
     options.addOption(bool, "static", static);
     options.addOption([]const u8, "dll_name", "game");
 
-    // DEPENDENCIES
+    // DEPENDENCY
     const raylib = blk: {
         const raylib_dep = b.dependency("raylib", .{
             .target = target,
@@ -21,11 +26,69 @@ pub fn build(b: *std.Build) void {
         });
         break :blk raylib_dep.artifact("raylib");
     };
-    if (!static) {
-        b.installArtifact(raylib);
+
+    // COMPILE
+    if (target.result.isWasm()) {
+        try buildWeb(b, target, optimize);
+    } else {
+        const exe = if (static) blk: {
+            break :blk buildStaticNative(b, target, optimize, raylib, options);
+        } else blk: {
+            const dll = buildGameLib(b, target, optimize, raylib, options);
+            const dll_install = b.addInstallArtifact(dll, .{});
+
+            const reload_step = b.step("reload", "Build the dll");
+            reload_step.dependOn(&dll_install.step);
+
+            const exe = buildDynamicNative(b, target, optimize, raylib, options);
+            exe.step.dependOn(&dll_install.step);
+            break :blk exe;
+        };
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
     }
 
-    // COMPILE STEPS
+    // TODO: Put test step back.
+}
+
+fn buildStaticNative(b: *Build, target: ResolvedTarget, optimize: OptimizeMode, raylib: *Compile, options: *Options) *Compile {
+    const exe = b.addExecutable(.{
+        .name = "static",
+        .root_source_file = b.path("src/desktop.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.linkLibrary(raylib);
+    exe.root_module.addOptions("config", options);
+
+    return exe;
+}
+
+fn buildDynamicNative(b: *Build, target: ResolvedTarget, optimize: OptimizeMode, raylib: *Compile, options: *Options) *Compile {
+    const exe = b.addExecutable(.{
+        .name = "dynamic",
+        .root_source_file = b.path("src/desktop.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.linkLibrary(raylib);
+    exe.root_module.addOptions("config", options);
+
+    b.installArtifact(raylib);
+
+    return exe;
+}
+
+fn buildGameLib(b: *Build, target: ResolvedTarget, optimize: OptimizeMode, raylib: *Compile, options: *Options) *Compile {
     var dll = b.addSharedLibrary(.{
         .name = "game",
         .root_source_file = b.path("src/Game.zig"),
@@ -33,35 +96,15 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     dll.linkLibrary(raylib);
-    const dll_install = b.addInstallArtifact(dll, .{});
+    dll.root_module.addOptions("config", options);
 
-    const reload_step = b.step("reload", "Build the dll");
-    reload_step.dependOn(&dll_install.step);
+    return dll;
+}
 
-    const exe = b.addExecutable(.{
-        .name = if (static) "static" else "dynamic",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.linkLibrary(raylib);
-    exe.root_module.addOptions("config", options);
-    if (!static) {
-        exe.step.dependOn(&dll_install.step);
-    }
-    b.installArtifact(exe);
-
-    // RUN STEPS
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // TESTS
-    // TODO: Put test step back.
+/// Unimplemented
+fn buildWeb(b: *Build, target: ResolvedTarget, optimize: OptimizeMode) !void {
+    _ = b;
+    _ = target;
+    _ = optimize;
+    @panic("Unimplemented");
 }
