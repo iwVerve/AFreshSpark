634a547faf4d85803742589475b8c030ae6d432b
iwVerve <thedolphiner@gmail.com>
2024-08-25 10:12:18 +0200

    warp exit puzzle

--- src/Game.zig
+++ src/Game.zig
@@ -40,7 +40,7 @@ pub fn init(self: *Game, init_window: bool) !void {
 
     try self.assets.init();
 
-    const level = try LevelState.init(self.allocator, &levels.sandbox, &self.assets);
+    const level = try LevelState.init(self.allocator, &levels.warp_exit, &self.assets);
     self.state = .{ .level = level };
 }
 
