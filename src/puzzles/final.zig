d8df885db0253c5dac9864b53a79f80c719605d3
iwVerve <thedolphiner@gmail.com>
2024-08-26 11:01:30 +0200

    warp intro 2 puzzle

--- src/puzzles/final.zig
+++ src/puzzles/final.zig
@@ -1,6 +1,9 @@
 const ray = @import("../raylib.zig");
 
-pub const colors = .{};
+pub const colors = .{
+    .background = .{ .r = 239, .g = 40, .b = 206, .a = 255 },
+    .foreground = .{ .r = 119, .g = 20, .b = 36, .a = 255 },
+};
 pub const exit = .{ .x = 0, .y = 0 };
 pub const exit_direction = .right;
 
--- src/puzzles/template.zig
+++ src/puzzles/template.zig
@@ -1,6 +1,9 @@
 const ray = @import("../raylib.zig");
 
-pub const colors = .{};
+pub const colors = .{
+    .background = .{ .r = 40, .g = 240, .b = 80, .a = 255 },
+    .foreground = .{ .r = 20, .g = 120, .b = 40, .a = 255 },
+};
 pub const exit = .{ .x = 0, .y = 0 };
 pub const exit_direction = .right;
 
